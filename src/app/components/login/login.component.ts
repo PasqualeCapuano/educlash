38fb32ea7602890b871588a0c428b2e2d2519922
FrancescoBossoli <113616058+FrancescoBossoli@users.noreply.github.com>
2023-12-02 15:30:41 +0100

    Connessione tra Componente di Login, AuthService e Firebase
    Aggiunta trasmissione dei dati del form all'AuthService, che dialogando con Firebase modifica la rotta o restituisce un errore in console

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterLink} from "@angular/router";
+import { Router } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    imports: [CommonModule, FormsModule],
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+    email: string = '';
+    password: string = '';
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    login() {
+        this.authService.login({email: this.email, password: this.password}).subscribe({
+            next: (data) => {
+                console.log(data);
+                this.router.navigate(['home/homepage']);
+            },
+            error: (error) => {
+                console.log(error.message);
+            }
+        })
+    }
 }
+
+
